b535c42aa75259fd4d88c3d242802e473356b49e
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-08-19 19:43:35 +0600

    modify deposit calculation

--- js/deposit.js
+++ js/deposit.js
@@ -1,15 +1,36 @@
 
-// add event listener to the deposit btn
+// step1 add event listener to the deposit btn
 document.getElementById('btn-deposit').addEventListener('click' , function(){
-    // get the deposit amount from the deposit field
-    const depositField = document.getElementById('deposit-field')
-    const depositAmount  = depositField.value ;
-    console.log(depositAmount)
 
-    // get the current deposit total amount
+    // step2 get the deposit amount from the deposit field
+    const depositField = document.getElementById('deposit-field')
+    const newDepositAmountString  = depositField.value ;
+    const newDepositAmount = parseFloat(newDepositAmountString)
+
+
+    //step3 get the current deposit total amount
 
     const depositTotalElement = document.getElementById('deposit-total')
-    const depositTotal = depositTotalElement.innerText
-    depositTotalElement.innerText = depositAmount
-    console.log(depositTotal)
+    const previousDepositTotalString = depositTotalElement.innerText
+    const previousDepositTotal = parseFloat(previousDepositTotalString)
+
+
+    //step4 add number to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount ;
+    depositTotalElement.innerText = currentDepositTotal
+    console.log(previousDepositTotal)
+
+    //step5 get  balance of current  total
+    const balanceTotalElement = document.getElementById('balance-total')
+    const prevBalanceTotalString = balanceTotalElement.innerText ;
+    const prevBalanceTotal = parseFloat(prevBalanceTotalString)
+    
+
+    // step-6 calculate current balance total
+    const currentBalanceTotal = prevBalanceTotal + newDepositAmount ;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+// step7 clear the deposit value
+    depositField.value = ''
 })
